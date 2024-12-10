0640ea6bb1e6f70818a4ddfae535ab581658840a
Rodrigo ROG Z <rorevillaca@gmail.com>
2024-12-10 23:52:14 +0100

    Jsonified search results

--- searchEngine/SearchEngine.py
+++ searchEngine/SearchEngine.py
@@ -3,10 +3,26 @@ import openai
 import numpy as np
 import pickle
 import faiss
+import json
+import os
+
+
+def load_env_secret(filepath="./searchEngine/key.env"):
+    if os.path.exists(filepath):  # Check if the file exists
+        with open(filepath) as f:
+            for line in f:
+                if line.strip() and not line.startswith("#"):  # Skip blank lines and comments
+                    key, value = line.strip().split("=", 1)
+                    os.environ[key] = value
+
+# Load the secret
+load_env_secret()
+SECRET = os.getenv("SECRET") 
+
 
 class SearchEngine:
     def __init__(self):
-        self.key = ""  
+        self.key = SECRET
         self._titles = self.load_from_pickle("titles")
         self._oclc = self.load_from_pickle("oclc")
         self._covers = self.load_from_pickle("covers")
@@ -30,14 +46,17 @@ class SearchEngine:
         np_emb = np.asarray(embedding)
         return np_emb
     
-    def search(self, query, k=None, d=None):
+    def search(self, query, k=20, d=None):
         query_embedding = self.generate_embedding(query)
         reshaped_embedding = query_embedding.reshape(1, 1536)
         if not d is None:
             distances, indices = self.radius_search(reshaped_embedding, d, k)
         elif not k is None:
             distances, indices = self._index.search(reshaped_embedding, k) 
-        results = [self._titles[i]+ " | " + str(self._oclc[i]) + " | " + str(self._covers[i]) for j, i in enumerate(indices[0])]
+
+        results = [" | " + self._titles[i]+ " | " + str(self._oclc[i]) + " | " + str(self._covers[i]) for j, i in enumerate(indices[0])]
+        json_result = self.jsonify_search_result(indices[0])
+        print(json_result)
         return results
     
     def radius_search(self, reshaped_embedding, d, k):
@@ -45,7 +64,6 @@ class SearchEngine:
         if not k is None:
             n = k
         distances, indices = self._index.search(reshaped_embedding, n)
-        print(distances)
         filtered_indices = []
         filtered_distances = []
         for i in range(n):
@@ -53,5 +71,15 @@ class SearchEngine:
                 filtered_indices.append(indices[0][i])
                 filtered_distances.append(distances[0][i])
         filtered_indices = [filtered_indices]
-        print(filtered_distances)
         return filtered_distances, filtered_indices
+    
+    def jsonify_search_result(self, indices):
+        json_structure = [{
+            "topic": "search results",
+            "topic_id": "search_results",
+            "sub_topic": "search results",
+            "virtual_shelf_temp": 1,
+            "books_in_bookcase": len(indices),
+            "books": [{"OCLC": oclc, "cover_file": cover} for oclc, cover in zip(self._oclc[indices], self._covers[indices])]
+        }]
+        return json_structure
