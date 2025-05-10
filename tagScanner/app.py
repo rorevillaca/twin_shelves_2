cff4adfcc5682bfc2460cc96d978a012aa83b5ce
Rodrigo ROG Z <rorevillaca@gmail.com>
2025-05-10 20:18:30 +0200

    Add scanning logic

---
+++ tagScanner/app.py
@@ -0,0 +1,141 @@
+from flask import Flask, Response
+from smartcard.scard import *
+from smartcard.pcsc import *
+import smartcard.util
+from book_db import BookDB
+
+app = Flask(__name__)
+
+
+def event_stream():
+        """Continously checks if there is a stage change for the reader. 
+        This event stream is a generator that out puts the serial IDs when they arrive. 
+        Checking for state changes is blocking.
+        """
+        
+        try:
+            print('----- Trying to establish context with reader: --')
+
+            hresult, hcontext = SCardEstablishContext(SCARD_SCOPE_USER)
+            if hresult != SCARD_S_SUCCESS:
+                raise error(
+                    'Failed to establish context: ' +
+                    SCardGetErrorMessage(hresult))
+            
+            print('----- Context established! ----------------------')
+
+            try:
+                hresult, readers = SCardListReaders(hcontext, [])
+                if hresult != SCARD_S_SUCCESS:
+                    raise error(
+                        'Failed to list readers: ' +
+                        SCardGetErrorMessage(hresult))
+                
+                print('----- Found Readers! ------------------------')
+
+                print('PCSC Readers:', readers)
+
+                readerstates = []
+                for i in range(len(readers)):
+                    readerstates += [(readers[i], SCARD_STATE_UNAWARE)]
+
+
+                while True:
+
+                    print('----- Current reader and card states are: -------')
+                    hresult, newstates = SCardGetStatusChange(hcontext, 0, readerstates)
+                    for i in newstates:
+                        print(i)
+
+
+                    
+                    print('----- Please insert or remove a card ------------')
+                    hresult, newstates = SCardGetStatusChange(
+                                            hcontext,
+                                            INFINITE,
+                                            newstates)
+
+                    print('----- New reader and card states are: -----------')
+                    for i in newstates:
+                        print(i)
+
+                        _x, eventstate, _y = i
+
+                        if eventstate & SCARD_STATE_PRESENT:
+                            hresult, hcard, dwActiveProtocol = SCardConnect(
+                                        hcontext,
+                                        readers[0],
+                                        SCARD_SHARE_SHARED,
+                                        SCARD_PROTOCOL_T0 | SCARD_PROTOCOL_T1)
+                            hresult, response_ID = SCardTransmit(hcard,dwActiveProtocol,[0xFF,0xCA,0x00,0x00,0x00])
+
+                            print('----- FOUND ID ------------------------------')
+                            
+                            print(smartcard.util.toHexString(response_ID[:-2], smartcard.util.HEX))
+                            # yield 'data: %s\n\n' % (smartcard.util.toHexString(response[:-2], smartcard.util.HEX))
+
+                            hresult, hcard, dwActiveProtocol = SCardConnect(
+                                        hcontext,
+                                        readers[0],
+                                        SCARD_SHARE_SHARED,
+                                        SCARD_PROTOCOL_T0 | SCARD_PROTOCOL_T1)
+                            hresult, response_DATA = SCardTransmit(hcard,dwActiveProtocol,[0xFF,0xB0,0x00,0x03,0x00])
+                            if hresult != SCARD_S_SUCCESS:
+                                raise PCSCExceptions.BaseSCardException(hresult)
+                            print('----- FOUND DATA ------------------------------')
+
+                            smartcard_content = smartcard.util.toHexString(response_DATA[:-2], smartcard.util.HEX)
+                            book_barcode = db.transform_hex_to_barcode(smartcard_content)
+                            book_oclc = db.get_book_oclc(book_barcode)
+                            print(f"Barcode: {book_barcode}, OCLC: {book_oclc}")
+                            yield f'data: {book_oclc}\n\n'
+
+            except error as e:
+                print(e)
+
+            finally:
+                hresult = SCardReleaseContext(hcontext)
+                if hresult != SCARD_S_SUCCESS:
+                    raise error(
+                        'Failed to release context: ' +
+                        SCardGetErrorMessage(hresult))
+                print('Released context.')
+
+        except error as e:
+            print(e)
+
+@app.route('/stream')
+def stream():
+    """Entry point for a new stream"""
+    print('----- New Stream Connection: --------------------')
+
+    return Response(event_stream(),
+                          mimetype="text/event-stream")
+
+@app.route('/')
+def home():
+    """"Quick Demo showcase usage of the stream using JavaScript"""
+    return """
+        <!doctype html>
+        <pre id="out"></pre>
+        <script>
+            function sse() {
+                var source = new EventSource('/stream');
+                var out = document.getElementById('out');
+                source.onmessage = function(e) {
+                    // XSS in chat is fun (let's prevent that)
+                    out.textContent =  e.data + '\\n' + out.textContent;
+                };
+            }
+            sse();
+        </script>
+
+    """ 
+
+
+if __name__ == '__main__':
+    db = BookDB()
+    app.debug = True
+    app.run(threaded=True)
+
+
---
+++ tagScanner/book_db.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+class BookDB:
+    def __init__(self):
+        bookswall = pd.read_csv("./data/bookswall.csv", dtype={"OCLC Number": str, "LHR Item Barcode": str})
+        recommendations = pd.read_csv("./data/recommendationArea.csv", dtype={"OCLC Number": str, "LHR Item Barcode": str})
+        db = pd.concat([bookswall, recommendations], ignore_index=True)
+        db = db.rename(columns={'LHR Item Barcode': 'barcode','OCLC Number': 'oclc'})
+        db = db[['barcode', 'oclc']]
+        self.db = dict(zip(db['barcode'], db['oclc']))
+        del bookswall, recommendations, db
+        print(f"Succesfully loaded {len(self.db)} barcode/OCLC pairs")
+
+    def get_book_oclc(self, barcode):
+        return self.db[barcode]
+
+    @staticmethod
+    def transform_hex_to_barcode(hex_string):
+        chars = [
+            part[-2:] for part in hex_string.strip().split()
+        ]
+        return ''.join(c for c in ''.join(chars) if c.isdigit())
